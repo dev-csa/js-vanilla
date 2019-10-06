6b34ba157059fa214e0255c11b399e32ff75fb49
CSA <csarising@gmail.com>
2019-10-06 21:44:36 +0900

    upload

---
+++ todo.js
@@ -0,0 +1,57 @@
+const toDoForm = document.querySelector(".js-toDoForm");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos';
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const delBtn = document.createElement("button");
+  const newId = toDos.length + 1;
+  span.innerText = text;
+  delBtn.innerText = "X";
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  li.id = newId;
+  toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  console.log(currentValue);
+  paintToDo(currentValue);
+  toDoInput.value = "";
+
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if(loadedToDos !== null){
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(item){
+      paintToDo(item.text);
+    });
+  }
+
+}
+
+
+function init(){
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
